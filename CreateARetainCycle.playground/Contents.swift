14525082206cc4b318c267e5dfa02bb243a221ab
Anil <anilMobileDevelopement@users.noreply.github.com>
2017-08-21 23:06:53 +0530

    update retain cycle

--- CreateARetainCycle.playground/Contents.swift
+++ CreateARetainCycle.playground/Contents.swift
@@ -1,4 +1,24 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    deinit {
+        print("\(name) is being deinitialisation")
+    }
+}
+class Apartment {
+    let number: Int
+    weak var tenant: Person?
+    init(number: Int) {
+        self.number = number
+    }
+}
+var bob:Person? = Person(name : "Bob")
+var apartment = Apartment(number: 232)
+apartment.tenant = bob
+bob = nil
+
 
